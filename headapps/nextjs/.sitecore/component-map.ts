3f3fae246768c4382b9fb77301e4413c65f6d147
zescabedo <zescabedo@gmail.com>
2025-10-05 20:23:54 -0500

    feat: add Content SDK app under headapps/nextjs

---
+++ headapps/nextjs/.sitecore/component-map.ts
@@ -0,0 +1,13 @@
+// Below are built-in components that are available in the app, it's recommended to keep them as is
+import { BYOCWrapper, NextjsContentSdkComponent, FEaaSWrapper } from '@sitecore-content-sdk/nextjs';
+import { Form } from '@sitecore-content-sdk/nextjs';
+// end of built-in components
+
+// Components must be registered within the map to match the string key with component name in Sitecore
+export const componentMap = new Map<string, NextjsContentSdkComponent>([
+  ['BYOCWrapper', BYOCWrapper],
+  ['FEaaSWrapper', FEaaSWrapper],
+  ['Form', Form],
+]);
+
+export default componentMap;
